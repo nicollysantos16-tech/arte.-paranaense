1fbe67deb2738e67829b74c4af8a7e601e95abec
nicollysantos16-tech <nicolly.santos16@escola.pr.gov.br>
2026-09-15 11:15:27 -0300

    atualizei o cod

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+cont botoesCurtir = document. querySelectorAll(".curtir");
+botoesCurtir. forEach(function(botoesCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir)
+    function curtir (){
+        const contador = botaoCurtir . querySelector("span");
+        if (curtiu === false){
+            contador . textContent++;
+            curtiu = true;
+            else {
+                con.textContent--;
+             curtiu = false;
+        }  
+ }
+});
